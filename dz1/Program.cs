562feef12391cba39234e99cb09c55ea0b15cda9
кристина <kristytitova@mail.ru>
2022-08-24 11:02:40 +0300

    Задача 41

---
+++ dz1/Program.cs
@@ -0,0 +1,17 @@
+﻿// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+// 1, -7, 567, 89, 223-> 4
+
+Console.Write(value: "Введи элементы(через пробел): ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+ 
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        count++;
+    }
+}
+ 
+Console.WriteLine($"Кол-во элементов >: {count}");
